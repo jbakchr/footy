df6de5a0d6222ed13be7947ea5d5bf55a2f81f78
Jonas Bak Phillipson <jbakchr@gmail.com>
2025-04-23 09:44:59 +0200

    Added initial fetch of league standing

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 #!/usr/bin/env node
 
+require("dotenv").config();
 const { program } = require("commander");
-const { version } = require("./package.json");
+const axios = require("axios");
+
 const leagues = require("./leagues/leagues");
+const { version } = require("./package.json");
 
 program
   .name("footy")
   .description("CLI for showing football league standings in the terminal")
   .version(version)
   .argument("<league>", "league to show standings for")
-  .action((league) => {
-    const leagueId = leagues[league];
-    console.log(leagueId);
+  .action(async (league) => {
+    const options = {
+      method: "GET",
+      url: "https://free-api-live-football-data.p.rapidapi.com/football-get-standing-all",
+      params: { leagueid: leagues[league] },
+      headers: {
+        "x-rapidapi-key": process.env.API_KEY,
+        "x-rapidapi-host": "free-api-live-football-data.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data.response.standing);
+    } catch (error) {
+      console.log(error);
+    }
   });
 
 program.parse();
