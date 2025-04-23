55bc9b087b67039d31e1ab731912a9627d248d09
Jonas Bak Phillipson <jbakchr@gmail.com>
2025-04-23 11:43:03 +0200

    Added table output

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require("dotenv").config();
 const { program } = require("commander");
 const axios = require("axios");
+const { Table } = require("console-table-printer");
 
 const leagues = require("./leagues/leagues");
 const { version } = require("./package.json");
@@ -25,7 +26,34 @@ program
 
     try {
       const response = await axios.request(options);
-      console.log(response.data.response.standing);
+
+      const p = new Table({
+        columns: [
+          { name: "#", alignment: "right" }, // with alignment and color
+          { name: "Team", alignment: "left" },
+          { name: "Pl", alignment: "right" },
+          { name: "W", alignment: "right" },
+          { name: "D", alignment: "right" },
+          { name: "L", alignment: "right" },
+          { name: "Pts", alignment: "right", color: "yellow" },
+        ],
+      });
+
+      response.data.response.standing.forEach(
+        ({ idx, name, played, wins, draws, losses, pts }, i) => {
+          p.addRow({
+            "#": idx,
+            Team: name,
+            Pl: played,
+            W: wins,
+            D: draws,
+            L: losses,
+            Pts: pts,
+          });
+        }
+      );
+
+      p.printTable();
     } catch (error) {
       console.log(error);
     }
